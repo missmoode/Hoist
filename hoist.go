a7f8c97019f35d3e8f5bdd29c0ad8076e7ae9b28
Amelia <zewlzor+ame@gmail.com>
2018-10-29 20:50:07 +0000

    bluh

--- hoist.go
+++ hoist.go
@@ -1,1 +1,31 @@
 package main
+
+//bonk
+
+import (
+  "github.com/spf13/viper"
+  "fmt"
+  "os"
+)
+
+func main() {
+  // Configuration Defaults
+  viper.SetDefault("database", map[string]string{"type": "postgres", "connectionString": "user=lunar password=password dbname=lunar host=localhost sslmode=disable"})
+  viper.SetDefault("database.user", "hoist")
+  viper.SetDefault("database.password", "password")
+  viper.SetDefault("database.dbname", "hoist")
+  viper.SetDefault("database.sslmode", "disable")
+  viper.SetDefault("host", "localhost")
+
+  //Make configuration if it doesn't exist and read it
+  viper.SetConfigName("config")
+  cwd, err := os.Getwd()
+  if err != nil {
+    panic(fmt.Errorf("Couldn't get current working directory: %s\n", err.Error()))
+  }
+  viper.AddConfigPath(cwd)
+  err = viper.ReadInConfig()
+  if err != nil {
+    panic(fmt.Errorf("Couldn't load config: %s \n", err))
+  }
+}
