64f3dfec4b14bd300e5e6916eae5e5ddb03348c1
Amy Harris <42555115+amelinya@users.noreply.github.com>
2018-10-31 20:27:59 +0000

    restart in a language i'm actually good at - nodejs

--- authenticator.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func getClientSecret(instanceURL string) {
-
-}
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get -v ./...
-mkdir target
-cd target
-gox -os="linux windows darwin" -arch="amd64 386"  -ldflags "-X main.Rev=`git rev-parse --short HEAD`" -verbose ../...;
