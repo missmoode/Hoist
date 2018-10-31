03068483e539db90e4bb277e9bc66274b434d431
Amy Harris <42555115+amelinya@users.noreply.github.com>
2018-10-31 01:19:11 +0000

    bunch of work

--- hoist.go
+++ hoist.go
@@ -6,7 +6,20 @@ import (
   "github.com/spf13/viper"
   "fmt"
   "os"
+  "strings"
+  "github.com/jinzhu/gorm"
+  "github.com/gin-gonic/gin"
+  "net/http"
 )
+
+
+// Mastodon instance certificate
+type Instance struct {
+  gorm.Model
+  Address   string `gorm:"primary_key"`
+  Secret    string
+}
+
 
 func main() {
   // Configuration Defaults
@@ -19,7 +32,7 @@ func main() {
   viper.SetDefault("host", "localhost")
   viper.SetDefault("port", 81)
 
-  //Make configuration if it doesn't exist and read it
+  // Make configuration if it doesn't exist and read it
   viper.SetConfigName("config")
   cwd, err := os.Getwd()
   if err != nil {
@@ -33,6 +46,34 @@ func main() {
 
   host := viper.GetString("host")
   port := viper.getInt("port")
+  siteURL := viper.GetString("url")
+  databaseConfig := viper.GetStringMapString("database")
 
-  siteURL = viper.GetString("url")
+  // Connect to database
+  db, err := gorm.Open(databaseConfig["type"], databaseConfig["connectionString"])
+  if err != nil {
+    panic(fmt.Errorf("Couldn't open database connection: %s \n", err))
+  }
+  defer db.Close()
+
+  db.AutoMigrate(&Instance{})
+
+  router := gin.Default()
+
+  router.GET("/auth/:instance`", func (c *gin.Context) {
+    address := strings.ToLower(c.Param("instance"))
+
+    var instance Instance
+    db.First(&instance, "address = ?", address)
+
+    if db.Address == "" {
+      response, err := http.Get(fmt.Sprintf(""))
+    }
+  })
 }
+
+// TODO: SSL cert
+
+/*GRRR going to have to build a workaround in the server code to try both HTTP and HTTPS because mastodon admins are too cheap to buy SSL certs
+too late to get started on that now it'll take like two hours
+*/
