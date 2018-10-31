4ed6981292685ff03ebee0866cd3209d22022d3f
Amelia <42555115+amelinya@users.noreply.github.com>
2018-10-31 04:48:21 +0000

    Move config code to own file

--- hoist.go
+++ hoist.go
@@ -2,8 +2,8 @@ package main
 
 //bonk
 
+import "github.com/spf13/viper"
 import (
-  "github.com/spf13/viper"
   "fmt"
   "os"
   "strings"
@@ -22,27 +22,7 @@ type Instance struct {
 
 
 func main() {
-  // Configuration Defaults
-  viper.SetDefault("database", map[string]string{"type": "postgres", "connectionString": "user=lunar password=password dbname=lunar host=localhost sslmode=disable"})
-  viper.SetDefault("database.user", "hoist")
-  viper.SetDefault("database.password", "password")
-  viper.SetDefault("database.dbname", "hoist")
-  viper.SetDefault("database.sslmode", "disable")
-  viper.SetDefault("url", "http://hoist.amy.zone")
-  viper.SetDefault("host", "localhost")
-  viper.SetDefault("port", 81)
-
-  // Make configuration if it doesn't exist and read it
-  viper.SetConfigName("config")
-  cwd, err := os.Getwd()
-  if err != nil {
-    panic(fmt.Errorf("Couldn't get current working directory: %s\n", err.Error()))
-  }
-  viper.AddConfigPath(cwd)
-  err = viper.ReadInConfig()
-  if err != nil {
-    panic(fmt.Errorf("Couldn't load config: %s \n", err))
-  }
+  loadConfig()
 
   host := viper.GetString("host")
   port := viper.getInt("port")
