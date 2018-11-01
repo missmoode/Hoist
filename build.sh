1cec5e018ec425aa05b990de297959dfad61dcb8
Amy Harris <42555115+amelinya@users.noreply.github.com>
2018-11-01 01:10:07 +0000

    Add 64-bit build target

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 babel src --presets=@babel/preset-env --out-dir lib
-pkg --targets latest-linux-x86 --out-path dist/ ./lib/hoist.js
+pkg --targets latest-linux-x86,latest-linux-x64 --out-path dist/ ./lib/hoist.js
