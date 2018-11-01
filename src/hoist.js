80c3f2abcb72298ea57c6eaa9c5a000a7b9097be
Amelia <42555115+amelinya@users.noreply.github.com>
2018-11-01 21:04:47 +0000

    SSL

--- src/hoist.js
+++ src/hoist.js
@@ -9,18 +9,14 @@ import Database from './database';
 
 
 class App {
-    constructor(port, dbHost, dbName, dbUsername, dbPassword) {
+    constructor(credentials, dbHost, dbName, dbUsername, dbPassword) {
         this.express = Express()
 
         this.express.use('/authentication', authentication)
 
-        this.express.listen(80, () => {
-            console.log(`App listening on port ${port}`)
-        })
+        this.httpServer = https.createServer(credentials, this.express)
 
-        this.httpsServer = https.createServer(credentials, this.express)
-
-        this.httpsServer.listen(443, () => {
+        this.httpServer.listen(443, () => {
             console.log('HTTP server running on port 443')
         })
 
@@ -31,9 +27,8 @@ class App {
 var config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 export default new App(
     {
-        key: fs.readFileSync(config.credentials.privateKey, 'utf8'),
-        cert: fs.readFileSync(config.credentials.certificate, 'utf8'),
-        ca: fs.readFileSync(config.credentials.ca, 'utf8')
+        key: fs.readFileSync(config.sslPath + 'privkey.pem', 'utf8'),
+        cert: fs.readFileSync(config.sslPath + 'fullchain.pem', 'utf8')
     },
     config.database.host, 
     config.database.name, 
