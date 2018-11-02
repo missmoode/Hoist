1629666318c388cf995c354a7cbb14f4c67406ad
Amelia <42555115+amelinya@users.noreply.github.com>
2018-11-02 22:42:03 +0000

    bluh

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -4,6 +4,12 @@ import getInstance from '../lib/clientProvider';
 const router = express.Router()
 
 router.get("/authorize", (req, res) => {
+    if (req.query.error != null) {
+        
+    } else {
+
+    }
+    req.query.code
     res.status(200).json({})
 })
 
